31d2787c817ec8057f863960ab382531be2c880a
InsomniaKitten <admin@designdevils.co.uk>
2017-10-05 20:36:15 +0100

    Backport to 1.10.2

--- src/main/java/net/insomniakitten/ast/ArmorSoundTweak.java
+++ src/main/java/net/insomniakitten/ast/ArmorSoundTweak.java
@@ -1,17 +1,18 @@
 package net.insomniakitten.ast;
 
-import com.google.common.collect.ImmutableList;
 import net.minecraft.client.Minecraft;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.EntityEquipmentSlot;
-import net.minecraft.item.ItemArmor;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.SoundCategory;
-import net.minecraftforge.event.entity.living.LivingEquipmentChangeEvent;
+import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Mod(modid = ArmorSoundTweak.MOD_ID,
      name = ArmorSoundTweak.MOD_NAME,
@@ -24,32 +25,28 @@ public class ArmorSoundTweak {
     public static final String MOD_ID = "armorsoundtweak";
     public static final String MOD_NAME = "Armor Sound Tweak";
     public static final String MOD_VERSION = "%VERSION%";
-    public static final String MC_VERSIONS = "[1.11,1.13)";
+    public static final String MC_VERSIONS = "[1.10,1.11)";
 
-    private static final ImmutableList<EntityEquipmentSlot> ARMOR_SLOTS = ImmutableList.of(
-            EntityEquipmentSlot.HEAD,
-            EntityEquipmentSlot.CHEST,
-            EntityEquipmentSlot.LEGS,
-            EntityEquipmentSlot.FEET
-    );
+    private static List<ItemStack> lastEquipment = new ArrayList<>();
 
     @SubscribeEvent
     @SideOnly(Side.CLIENT)
-    public static void onLivingEquipmentChange(LivingEquipmentChangeEvent event) {
-        if (Minecraft.getMinecraft().currentScreen != null && event.getEntity() instanceof EntityPlayer) {
-            if (ARMOR_SLOTS.contains(event.getSlot())
-                    && (event.getFrom().getItem() instanceof ItemArmor
-                    || event.getTo().getItem() instanceof ItemArmor)) {
-                EntityPlayer player = (EntityPlayer) event.getEntity();
-                ItemStack armor = event.getFrom().isEmpty() ? event.getTo() : event.getFrom();
-                if (!armor.isEmpty()) {
-                    ItemArmor.ArmorMaterial material = ((ItemArmor) armor.getItem()).getArmorMaterial();
-                    if (!player.world.isRemote) {
-                        player.world.playSound(null, player.getPosition(), material.getSoundEvent(),
-                                SoundCategory.PLAYERS, 1.0f, 1.0f);
-                    }
+    public static void onClientTick(TickEvent.ClientTickEvent event) {
+        Minecraft mc = Minecraft.getMinecraft();
+        if (event.phase.equals(TickEvent.Phase.END) && mc.player != null) {
+            List<ItemStack> equipmentCache = new ArrayList<>();
+            for (ItemStack stack : mc.player.getArmorInventoryList()) {
+                if (stack != null) equipmentCache.add(stack);
+            }
+            if (lastEquipment.size() != equipmentCache.size()) {
+                if (mc.player.world.isRemote) {
+                    BlockPos pos = new BlockPos(mc.player.posX, mc.player.posY, mc.player.posZ);
+                    mc.player.world.playSound(mc.player, pos,
+                            SoundEvents.ITEM_ARMOR_EQUIP_GENERIC,
+                            SoundCategory.PLAYERS, 1.0f, 1.0f);
                 }
             }
+            lastEquipment = equipmentCache;
         }
     }
 
