4b0cbfbfbdca11e44a9bee7c3f2aa4fd68928b1d
InsomniaKitten <admin@designdevils.co.uk>
2017-12-05 14:07:09 +0000

    Cleanup

--- src/main/java/net/insomniakitten/ast/ArmorSoundTweak.java
+++ src/main/java/net/insomniakitten/ast/ArmorSoundTweak.java
@@ -13,6 +13,7 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.fml.client.FMLClientHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -24,26 +25,28 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 
-@Mod(modid = ArmorSoundTweak.ID,
-     name = ArmorSoundTweak.NAME,
-     version = ArmorSoundTweak.VERSION,
-     acceptedMinecraftVersions = ArmorSoundTweak.MC_VERSIONS,
-     clientSideOnly = true)
+@Mod(modid = ArmorSoundTweak.ID, name = ArmorSoundTweak.NAME, version = ArmorSoundTweak.VERSION, clientSideOnly = true)
 @Mod.EventBusSubscriber(Side.CLIENT)
-public class ArmorSoundTweak {
+public final class ArmorSoundTweak {
 
     public static final String ID = "armorsoundtweak";
     public static final String NAME = "Armor Sound Tweak";
     public static final String VERSION = "%VERSION%";
-    public static final String MC_VERSIONS = "[1.10,1.13)";
 
     private static SoundEvent soundElytraEquip;
     private static List<ItemStack> lastEquipment = Lists.newArrayList();
 
+    @Mod.EventHandler
+    public void onPostInit(FMLPostInitializationEvent event) {
+        ResourceLocation id = new ResourceLocation("item.armor.equip_elytra");
+        soundElytraEquip = Optional.ofNullable(SoundEvent.REGISTRY.getObject(id))
+                .orElse(SoundEvents.ITEM_ARMOR_EQUIP_GENERIC);
+    }
+
     @SubscribeEvent
     @SideOnly(Side.CLIENT)
     public static void onClientTick(TickEvent.ClientTickEvent event) {
-        Minecraft mc = Minecraft.getMinecraft();
+        Minecraft mc = FMLClientHandler.instance().getClient();
 
         if (event.phase == TickEvent.Phase.START && mc.player != null) {
             List<ItemStack> equipment = Lists.newArrayList();
@@ -91,16 +94,8 @@ public class ArmorSoundTweak {
         }
 
         if (sound != null) {
-            player.world.playSound(player, new BlockPos(player),
-                    sound, SoundCategory.PLAYERS, 1.0f, 1.0f);
+            player.world.playSound(player, new BlockPos(player), sound, SoundCategory.PLAYERS, 1.0F, 1.0F);
         }
-    }
-
-    @Mod.EventHandler
-    public void onPostInit(FMLPostInitializationEvent event) {
-        ResourceLocation id = new ResourceLocation("item.armor.equip_elytra");
-        SoundEvent fallback = SoundEvents.ITEM_ARMOR_EQUIP_GENERIC;
-        soundElytraEquip = Optional.ofNullable(SoundEvent.REGISTRY.getObject(id)).orElse(fallback);
     }
 
 }
