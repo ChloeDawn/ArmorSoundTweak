1caa32a8bbf4585f7c134518163ccdc2ec786359
Chloe Dawn <chloe@sapphic.dev>
2020-12-30 20:37:44 +0000

    1.16.4  - Update Gradle setup  - Update dependencies  - Update JAR manifest  - Cleanup implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,56 +1,57 @@
-import java.time.Instant
-
 plugins {
-  id("fabric-loom") version "0.4.33"
+  id("fabric-loom") version "0.5.43"
   id("signing")
 }
 
 group = "dev.sapphic"
 version = "3.0.0"
 
-java {
-  sourceCompatibility = JavaVersion.VERSION_1_8
-  targetCompatibility = sourceCompatibility
-}
-
 dependencies {
-  minecraft("com.mojang:minecraft:1.16.1")
-  mappings("net.fabricmc:yarn:1.16.1+build.21:v2")
-  modImplementation("net.fabricmc:fabric-loader:0.9.0+build.204")
-  modImplementation(include(fabricApi.module("fabric-api-base", "0.16.0+build.384-1.16.1"))!!)
-  modImplementation(include(fabricApi.module("fabric-lifecycle-events-v1", "0.16.0+build.384-1.16.1"))!!)
+  minecraft("com.mojang:minecraft:1.16.4")
+  mappings(minecraft.officialMojangMappings())
+  modImplementation("net.fabricmc:fabric-loader:0.10.8")
+  modImplementation(include("net.fabricmc.fabric-api:fabric-api-base:0.2.0+ab87788d3a")!!)
+  modImplementation(include("net.fabricmc.fabric-api:fabric-lifecycle-events-v1:1.2.0+ffb68a873a")!!)
   implementation(include("com.electronwill.night-config:core:3.6.3")!!)
   implementation(include("com.electronwill.night-config:toml:3.6.3")!!)
-  implementation("org.checkerframework:checker-qual:3.5.0")
+  implementation("org.checkerframework:checker-qual:3.8.0")
 }
 
-tasks.withType<Jar> {
-  afterEvaluate {
-    archiveClassifier.set("fabric")
+tasks {
+  compileJava {
+    with(options) {
+      options.release.set(8)
+      isFork = true
+      isDeprecation = true
+      encoding = "UTF-8"
+      compilerArgs.addAll(listOf("-Xlint:all", "-parameters"))
+    }
   }
-  manifest.attributes(mapOf(
-    "Specification-Title" to project.name,
-    "Specification-Vendor" to project.group,
-    "Specification-Version" to 1,
-    "Implementation-Title" to project.name,
-    "Implementation-Version" to project.version,
-    "Implementation-Vendor" to project.group,
-    "Implementation-Timestamp" to Instant.now().toString()
-  ))
-}
 
-tasks.withType<JavaCompile> {
-  with(options) {
-    isFork = true
-    isDeprecation = true
-    encoding = "UTF-8"
-    compilerArgs.addAll(listOf(
-      "-Xlint:all", "-parameters"
+  processResources {
+    filesMatching("/fabric.mod.json") {
+      expand("version" to project.version)
+    }
+  }
+
+  jar {
+    // FIXME Loom does not respect this
+    archiveClassifier.set("fabric")
+
+    from("/LICENSE")
+
+    manifest.attributes(mapOf(
+      "Specification-Title" to "MinecraftMod",
+      "Specification-Vendor" to project.group,
+      "Specification-Version" to "1.0.0",
+      "Implementation-Title" to project.name,
+      "Implementation-Version" to project.version,
+      "Implementation-Vendor" to project.group,
+      "Sealed" to "true"
     ))
   }
 }
 
 signing {
-  useGpgCmd()
   sign(configurations.archives.get())
 }
--- src/main/java/dev/sapphic/armorsoundtweak/ArmorSoundTweak.java
+++ src/main/java/dev/sapphic/armorsoundtweak/ArmorSoundTweak.java
@@ -8,22 +8,24 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.block.AbstractSkullBlock;
-import net.minecraft.block.Blocks;
-import net.minecraft.client.gui.screen.ingame.ScreenHandlerProvider;
-import net.minecraft.item.ArmorItem;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.ElytraItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvent;
-import net.minecraft.sound.SoundEvents;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.ElytraItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.AbstractSkullBlock;
+import net.minecraft.world.level.block.Block;
+import org.checkerframework.checker.nullness.qual.Nullable;
 
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.function.Supplier;
@@ -36,28 +38,32 @@ public final class ArmorSoundTweak implements ClientModInitializer {
   private final Supplier<Boolean> pumpkins;
   private final Supplier<Boolean> anything;
 
-  private List<Item> oldEquipment = new ArrayList<>(0);
+  private List<Item> oldEquipment = Collections.emptyList();
 
   public ArmorSoundTweak() {
     final Path file = FabricLoader.getInstance().getConfigDir().resolve("armorsoundtweak.toml");
     final FileConfig config = FileConfig.builder(file).autoreload().build();
     final ConfigSpec spec = new ConfigSpec();
+
     spec.define("sounds.armor", true);
     spec.define("sounds.elytra", true);
     spec.define("sounds.skulls", false);
     spec.define("sounds.pumpkins", false);
     spec.define("sounds.anything", false);
+
     try {
       // No removal as config persists throughout entire runtime
       FileWatcher.defaultInstance().addWatch(file, () -> correct(config, spec));
     } catch (final IOException e) {
       throw new IllegalStateException("Unable to add config correction watcher", e);
     }
+
     this.armor = () -> config.getOrElse("sounds.armor", true);
     this.elytra = () -> config.getOrElse("sounds.elytra", true);
     this.skulls = () -> config.getOrElse("sounds.skulls", false);
     this.pumpkins = () -> config.getOrElse("sounds.pumpkins", false);
     this.anything = () -> config.getOrElse("sounds.anything", false);
+
     config.load();
     correct(config, spec);
   }
@@ -70,41 +76,65 @@ public final class ArmorSoundTweak implements ClientModInitializer {
   }
 
   private static boolean isSkull(final Item item) {
-    return (item instanceof BlockItem) && (((BlockItem) item).getBlock() instanceof AbstractSkullBlock);
+    if (item instanceof BlockItem) {
+      final Block block = ((BlockItem) item).getBlock();
+
+      return block instanceof AbstractSkullBlock;
+    }
+    return false;
   }
 
   @Override
   public void onInitializeClient() {
     ClientTickEvents.START_CLIENT_TICK.register(client -> {
-      if ((client.player != null) && (client.player.world != null) && client.player.world.isClient) {
+      if ((client.player != null) && (client.player.level != null) && client.player.level.isClientSide) {
         final List<Item> equipment = new ArrayList<>(4);
-        for (final ItemStack stack : client.player.getArmorItems()) {
+
+        for (final ItemStack stack : client.player.getArmorSlots()) {
           equipment.add(stack.getItem());
         }
-        if (client.currentScreen instanceof ScreenHandlerProvider<?>) {
+
+        if (client.screen instanceof AbstractContainerScreen<?>) {
           final Iterator<Item> newEquipment = equipment.iterator();
           final Iterator<Item> oldEquipment = this.oldEquipment.iterator();
+
           while (oldEquipment.hasNext() && newEquipment.hasNext()) {
             final Item newItem = newEquipment.next();
             final Item oldItem = oldEquipment.next();
+
             if (newItem != oldItem) {
-              final Item item = (newItem == Items.AIR) ? oldItem : newItem;
-              SoundEvent sound = SoundEvents.ITEM_ARMOR_EQUIP_GENERIC;
-              if (this.armor.get() && (item instanceof ArmorItem)) {
-                sound = ((ArmorItem) item).getMaterial().getEquipSound();
-              } else if (this.elytra.get() && (item instanceof ElytraItem)) {
-                sound = SoundEvents.ITEM_ARMOR_EQUIP_ELYTRA;
-              } else if (!this.pumpkins.get() || (item != Blocks.CARVED_PUMPKIN.asItem())) {
-                if ((!this.skulls.get() || !isSkull(item)) && !this.anything.get()) {
-                  continue;
-                }
+              final @Nullable SoundEvent sound = this.getEquipSound(newItem, oldItem);
+
+              if (sound != null) {
+                client.player.playNotifySound(sound, SoundSource.NEUTRAL, 1.0F, 1.0F);
               }
-              client.player.playSound(sound, SoundCategory.NEUTRAL, 1.0F, 1.0F);
             }
           }
         }
         this.oldEquipment = equipment;
       }
     });
+  }
+
+  private @Nullable SoundEvent getEquipSound(final Item newItem, final Item oldItem) {
+    final Item item = (newItem == Items.AIR) ? oldItem : newItem;
+
+    if (this.armor.get() && (item instanceof ArmorItem)) {
+      return ((ArmorItem) item).getMaterial().getEquipSound();
+    }
+
+    if (this.elytra.get() && (item instanceof ElytraItem)) {
+      return SoundEvents.ARMOR_EQUIP_ELYTRA;
+    }
+
+    if (this.pumpkins.get() && (item == Items.CARVED_PUMPKIN)) {
+      return SoundEvents.ARMOR_EQUIP_GENERIC;
+    }
+
+    if (this.anything.get() || (this.skulls.get() && isSkull(item))) {
+      return SoundEvents.ARMOR_EQUIP_GENERIC;
+    }
+
+    return null;
   }
 }
